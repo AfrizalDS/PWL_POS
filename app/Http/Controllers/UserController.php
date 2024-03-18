899c78abd36f3c0a6b734f56c7633e793b7b6455
AfrizalDS <angelicacoustarika@gmail.com>
2024-03-18 21:58:00 +0700

    praktikum 2.2_No found Exception_JS4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,33 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        //  $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);//tambah data ke tabel M_USER
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data);
-            
-        
-        
-        //JS 4
-
-         $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
-        
-        //coba akses usermodel
-        $user = UserModel::all();
+       
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
         
     }
