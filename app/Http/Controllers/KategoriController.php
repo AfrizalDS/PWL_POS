43ea2cf72d80116ec6838ed9e2e5ca684ede5c3f
AfrizalDS <angelicacoustarika@gmail.com>
2024-03-27 12:55:10 +0700

    Add JS 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,13 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
+
+
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
+       return $dataTable->render('kategori.index'); 
+        
+    }
+
+    public  function create()
+    {
+        return view('kategori.create');
+    }
+
+    
+    public function store(Request $request)
+{
+    $request->validate([
+        'kategori_kode' => 'required',
+        'kategori_nama' => 'required',
+    ]);
+
+    // Simpan data kategori
+    KategoriModel::create([
+        'kategori_kode' => $request->kategori_kode,
+        'kategori_nama' => $request->kategori_nama,
+    ]);
+
+    // Redirect ke halaman manajemen kategori
+    return redirect('/kategori')->with('success', 'Kategori berhasil dibuat.');
+}
+
+
+    public function edit($id){
+        $kategori = KategoriModel::find($id);
+        return view('kategori.edit', ['data' => $kategori]);
+    }
+
+    public function update(Request $request, $id){
+        $kategori = KategoriModel::find($id);
+        $kategori->kategori_kode = $request->kategori_kode;
+        $kategori->kategori_nama = $request->kategori_nama;
+        $kategori->save();
+        return redirect('/kategori');    
+    }
+    
+    public function delete($id)
+    {
+        KategoriModel::find($id)->delete();
+        return redirect('/kategori');
+    }
+
+  
+
+}
+
+
+// $data = [
         //     'kategori_kode' => 'SNK',
         //     'kategori_nama' => 'Snack\Makanan Ringan',
         //     'created_at' => now()
@@ -23,8 +78,5 @@ class KategoriController extends Controller
         // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
         // return 'Delete data berhasil. Jumlah data yang dihapus: ' .$row.'baris';
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
-    }
-
-}
+        // $data = DB::table('m_kategori')->get();
+        // return view('kategori', ['data' => $data]);
