f99cdbcf5fde9000f8f2e603178eedce91857728
AfrizalDS <angelicacoustarika@gmail.com>
2024-05-29 14:23:22 +0700

    Feat : Add JS 11 and dokumentation

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -9,39 +9,44 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    public function __invoke(Request $request){
-         //set validation
-         $validator = Validator::make($request->all(), [
+    // public function destroy(UserModel $user)
+    // {
+    //     $user->delete();
+    //     return response()->json([
+    //         'success' => true,
+    //         'message' => 'Data Berhasil Dihapus'
+    //     ]);
+    // }
+    public function __invoke(Request $request)
+    {
+        
+        $validator = Validator::make($request->all(), [
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
+            'level_id' => 'required', 
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
-        //if validations fails
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        //create user
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            'image' => $request->image->hashName()
         ]);
 
-        //return response JSON user is created
-        if ($user) {
-            return response()->json([
-                'success' => true,
-                'user' => $user,
-            ], 201);
-        }
-
-        //return JSON process insert failed
+        if($user){ return response()->json([
+            'success' => true,
+            'user' => $user,
+        ], 201);
+    }
         return response()->json([
-            'success' => false,
+            'status' => false,
         ], 409);
     }
 }
